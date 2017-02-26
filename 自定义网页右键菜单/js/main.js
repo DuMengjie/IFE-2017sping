8c0daa17d00a3abe3bd71f360b14cab9e5f6b154
DuMengjie <dumengjie2016@gmail.com>
2017-02-26 14:58:34 +0800

    自定义网页右键菜单

---
+++ 自定义网页右键菜单/js/main.js
@@ -0,0 +1,54 @@
+(function(){
+
+	var showArea = document.querySelector(".area"),
+		iMenu = document.querySelector("#imenu"),
+		areaX = showArea.offsetLeft,
+		areaY = showArea.offsetTop;
+
+	showArea.onmousedown = function(event){
+		var e = event || window.event;
+		if(e.button == "2") {
+			var	realX = e.clientX - areaX,
+				realY = e.clientY - areaY;
+
+			setPos(showArea, iMenu, realX, realY);
+		}
+	};
+
+	iMenu.onmousedown = function(event){
+		var e = event || window.event;
+		if(e.button == "2") {
+			//阻止事件冒泡
+			window.event? window.event.cancelBubble = true : e.stopPropagation();
+		} else {
+			// console.log(e.target);
+			alert(e.target.innerHTML);
+			iMenu.style.visibility = "hidden";
+		}
+	}
+
+	function setPos (eOut, eIn, x, y) {
+		var iInWidth = eIn.offsetWidth,
+			iInHeight = eIn.offsetHeight,
+			iOutWidth = Math.min(eOut.offsetWidth, document.body.clientWidth),		//取当前视口宽度（高度）和容器宽度（高度）的最小值
+			iOutHeight = Math.min(eOut.offsetHeight, document.body.clientHeight),
+			restWidth = iOutWidth - x,
+			restHeight = iOutHeight - y,
+			iPosX = x,
+			iPosY = y;
+
+		if(restWidth < iInWidth) {
+			iPosX -= iInWidth;
+		}
+
+		if(restHeight < iInHeight) {
+			iPosY -= iInHeight;
+		}
+
+		eIn.style.top = iPosY + 'px';
+		eIn.style.left = iPosX + 'px';
+		eIn.style.visibility = "visible";
+	}
+
+
+})();
